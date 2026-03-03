f290353083f755e8d0de6ab5e2d494f9889cc0fd
ng4mf <ngandhi3@seas.upenn.edu>
2026-03-02 22:13:05 -0800

    fixes for regularization sweep experiments

--- setup/thundercompute.sh
+++ setup/thundercompute.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+echo "================== APT Update =================="
 sudo apt update
-sudo apt install python3-pip
+echo "================== Install pip =================="
+sudo apt install -y python3-pip
+echo "================== Make virtual env =================="
 python3 -m venv venv
 source venv/bin/activate
+echo "================== Install pip packages =================="
 pip install -U torch torchvision torchaudio \
         --index-url https://download.pytorch.org/whl/cu121
 pip install -U matplotlib numpy datasets
+
+
+echo "================== Make jupyter kernel =================="
+pip install ipykernel
+python -m ipykernel install --user --name=venv --display-name "venvkernel"
